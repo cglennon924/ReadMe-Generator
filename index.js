571a1f78d2f0f663b91fc51caacd6d523eee89d3
cglennon924 <63322716+cglennon924@users.noreply.github.com>
2020-05-23 20:49:47 -0400

    added questions

--- index.js
+++ index.js
@@ -10,9 +10,64 @@ const prompt= [
     name:"name",
     message:"What is you username?"
 
-}
+},
 
-]
+{
+    type:"input",
+    name:"title",
+    message:"What is the title of your project?"
+
+},
+
+{   type:"input",
+    name:"reason",
+    message:"What's the reasoning for this project?"
+    
+},
+
+{
+    type:"input",
+    name:"lesson",
+    message:"What did you learn from this project? Give some examples please."
+},
+
+{
+    
+        type: "list",
+        name: "license",
+        message: "What licenses are you using?",
+        choices: ["MIT","GPL 3.0", "BSD 3", "None"],
+},
+    
+{
+        type: "list",
+        name: "node",
+        message: "Does this project require node.js to install?",
+        choices: ["Yes", "No"],
+        default: "Yes"
+},
+   
+{
+        type: "list",
+        name: "install",
+        choices: ["npm", "yarn"],
+        message: "If 'Yes', How would someone install your application?",
+        default: "npm"
+},
+
+{   type: "input", 
+    name: "factors",
+    message: "Did anyone help with this project?[if none press enter to skip]",
+    default: "None"
+},
+
+{
+    type: "list",
+    name: "requirements",
+    message: "Does this project require Axios, Inquire and/or other JSON package to run?",
+    choices: ["Yes","No"]
+}]
+
 
 //Function 1: Inquirer
 inquirer.prompt(prompt) 
